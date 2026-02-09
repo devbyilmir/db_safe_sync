c3ced9caa8be878c3452c3b34d5390452027c98a
Ilmir <devbyilmir@gmail.com>
2026-02-09 14:34:55 +0500

    add db connection and schema inspector

--- app/db/inspector.py
+++ app/db/inspector.py
@@ -1,6 +1,32 @@
+from sqlalchemy import inspect
+
+from app.db.models import TableSchema, ColumnSchema
+
+
 class SchemaInspector:
     def __init__(self, engine):
         self.engine = engine
+        self.inspector = inspect(engine)
 
     def get_schema(self):
-        raise NotImplementedError
+        tables = []
+
+        for table_name in self.inspector.get_table_names():
+            columns_info = self.inspector.get_columns(table_name)
+
+            columns = [
+                ColumnSchema(
+                    name=col["name"],
+                    type=str(col["type"])
+                )
+                for col in columns_info
+            ]
+
+            tables.append(
+                TableSchema(
+                    name=table_name,
+                    columns=columns
+                )
+            )
+
+        return tables
--- app/main.py
+++ app/main.py
@@ -1,5 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+from app.db.inspector import SchemaInspector
+
+
+load_dotenv()
+
+
 def main():
-    print("db_safe_sync started")
+    source_engine = create_engine(os.getenv("SOURCE_DB_URL"))
+    target_engine = create_engine(os.getenv("TARGET_DB_URL"))
+
+    source_inspector = SchemaInspector(source_engine)
+    target_inspector = SchemaInspector(target_engine)
+
+    source_schema = source_inspector.get_schema()
+    target_schema = target_inspector.get_schema()
+
+    print("SOURCE:", source_schema)
+    print("TARGET:", target_schema)
 
 
 if __name__ == "__main__":
